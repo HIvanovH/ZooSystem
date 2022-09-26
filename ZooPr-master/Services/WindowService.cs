4038af7c90af8db3c5b588652644bd7112d6eee0
HIvanovH <hristohristov1050@gmail.com>
2022-09-26 16:52:05 +0300

    .

---
+++ ZooPr-master/Services/WindowService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using Zoo.View_Models;
+
+namespace Zoo.Services
+{
+    //public delegate void LoginWindowService();
+    internal class WindowService 
+    {
+       
+        public void ShowMenuWindow()
+        {
+            var win = new Window();
+            win.Content = new MenuViewModel();
+            win.Show();
+
+        }
+        public void CloseLoginView()
+        {
+            Application.Current.MainWindow.Close();
+        }
+        
+    }
+}

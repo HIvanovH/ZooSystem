09baa4d8545c4ea426dc3c12ac9b10c6d8c4acfc
HIvanovH <hristohristov1050@gmail.com>
2022-09-27 14:36:59 +0300

    .

---
+++ Repository/Data/ZooDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Data
+{
+    public class ZooDbContext : DbContext
+    {
+        public ZooDbContext() : base(Properties.Settings.Default.DbConnect)
+        {
+
+        }
+        public DbSet<User> User { get; set; }
+        public DbSet<Animal> Animal { get; set; }
+        public DbSet<Category> Category { get; set; }
+        public DbSet<Event> Event { get; set; }
+        public DbSet<EventsType> EventsType { get; set; }
+        public DbSet<TicketsType> TicketsType { get; set; }
+
+        public DbSet<UserOrdercs> UserOrdercs { get; set; }
+    }
+}

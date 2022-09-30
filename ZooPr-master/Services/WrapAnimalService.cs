1f3fa3f726c120bc450fe43ec7b42a463dc56828
HIvanovH <hristohristov1050@gmail.com>
2022-09-30 14:02:56 +0300

    .

---
+++ ZooPr-master/Services/WrapAnimalService.cs
@@ -0,0 +1,35 @@
+﻿using Repository.Models;
+using Repository.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zoo.Models;
+
+namespace Zoo.Services
+{
+    public class WrapAnimalService
+    {
+        private static readonly WrapAnimalService _wrapAnimalService = new WrapAnimalService();
+        public static WrapAnimalService GetWrapAnimalService()
+        {
+            return _wrapAnimalService;
+        }
+        private WrapAnimalService()
+        {
+
+        }
+        public List<AnimalToDisplay> WrapAnimalToDisplay()
+        {
+            List<Animal> animalList = SearchForAnimalService.GetSearchForAnimalService().SearchAnimal();
+            List<AnimalToDisplay> result = new List<AnimalToDisplay>();
+            
+            foreach (Animal animal in animalList)
+            {
+                result.Add(new AnimalToDisplay(animal));
+            }
+            return result;
+        }
+    }
+}
